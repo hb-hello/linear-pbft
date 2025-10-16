ca3a4e24192b77cd01680e321ea9210f2c7e3b83
hb-hello <biyanihritwik@gmail.com>
2025-10-16 00:22:24 -0400

    Set up Maven dependencies

--- cli/src/main/java/org/example/CliApp.java
+++ cli/src/main/java/org/example/CliApp.java
@@ -1,8 +1,7 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class CliApp {
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -14,4 +13,5 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
+
 }
